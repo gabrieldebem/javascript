1ee9d7e00e523adcc4a3165216af7dcbc7de0c4f
gabrieldebem <gabrieldebem72@gmail.com>
2021-01-11 19:49:46 -0300

    var

--- intro/script.js
+++ intro/script.js
@@ -1,5 +1,16 @@
-alert('Bem vindo ao curso')
-var x = "gabriel"
-var y = " roxo"
-var sum = x+y
-document.getElementById('nome').value = sum
+//string
+var string = 'Curso de Javacript'
+
+//number
+var number = 32
+var numberFloat = 3.14
+
+//boolean
+var test = true //true = verdadeiro  false = falso
+
+//alert = abrir um dialog
+alert(numberFloat)
+//documente.write() = escreve algo ou alguma var no body
+document.write(string)
+
+console.log(number)
