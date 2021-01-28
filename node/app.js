31ac113d41a8f20dd8ae1aaaafc7320de63509e6
gabrieldebem <gabrieldebem72@gmail.com>
2021-01-28 11:14:36 -0300

    28/11

---
+++ node/app.js
@@ -0,0 +1,5 @@
+const http = require('http');
+http.createServer(function(requisicao, resposta){
+    resposta.end('Bem vindo ao meu site!');
+}).listen(8181)
+
--- node/intro.js
+++ node/intro.js
@@ -1,3 +1,17 @@
-const fs = require('fs')
+function soma(a,b){
+    return a+b;
+}
 
-console.log('Hello world')
+function sub(a,b){
+    return a-b;
+}
+
+function mult(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+console.log(div(14,17))
