b6310a9ace32acc42586a459dcaf519d43145017
Micaelle <suporte@i10digital.com.br>
2021-02-01 18:45:20 -0300

    operadores

--- intro/script.js
+++ intro/script.js
@@ -1,4 +1,14 @@
-var name = document.getElementById('name')
-var gift = document.getElementById('item')
+let num1 = prompt('Digite um valor')
+let num2 = prompt('Digite outro valor')
+num1 = parseInt(num1)
+num2 = parseInt(num2)
 
-document.write('<li>' + name + ' - ' + gift + '</li>')
+console.log('A soma entre ' + num1 + ' e ' + num2 + ' é igual à ' + (num1 + num2));
+console.log('A subtraçao entre ' + num1 + ' e ' + num2 + ' é igual à ' + (num1 - num2));
+console.log('A multiplicaçao entre ' + num1 + ' e ' + num2 + ' é igual à ' + (num1 * num2));
+console.log('A divisao entre ' + num1 + ' e ' + num2 + ' é igual à ' + (num1 / num2));
+console.log('O restante de ' + num1 + ' e ' + num2 + ' é igual à ' + (num1 % num2));
+console.log('A acreçao de ' + num1 + ' é igual à ' + (++num1));
+console.log(num1);
+console.log('A decreçao de ' + num1 + ' é igual à ' + (--num1));
+console.log(num1);
